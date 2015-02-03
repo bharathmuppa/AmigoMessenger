cd5e39ac27dba3126e56727415320ca58fb9abed
bharath <bharathmuppa@gmail.com>
2015-02-03 09:39:48 +0530

    image-upload

--- js/controllers/loginCtrl.js
+++ js/controllers/loginCtrl.js
@@ -2,11 +2,14 @@ chatApp.controller("loginCtrl", ["$scope", "$firebase", "commonDataFactory", "$h
 function($scope, $firebase, commonDataFactory, $http) {
 	$scope.loginDataFlag = true;
 	$scope.goToNext = function() {
+		
 		$scope.picDiv = true;
+		return false;
 	};
 	$scope.checkDetails = function(key) {
-		alert('asda');
+		
 		if ($scope.userName != null && $scope.userNum != null) {
+					
 			$scope.loginDataFlag = false;
 		} else {
 			$scope.loginDataFlag = true;
@@ -22,24 +25,40 @@ function($scope, $firebase, commonDataFactory, $http) {
 	
 	$('input[type=file]').change(function(e){
     var file = e.currentTarget.files[0];
+    mimeType=file.type;
+    console.log(mimeType);
+    if(mimeType.substr(0,5)=='image')
+{
 var contentAsBase64EncodedString;
 		var FR = new FileReader();
-
+ 
+ //this is callback for readAsDataURL(file)
 		FR.onload = function(encodedFile) {
-			console.log(encodedFile);
-			alert("onload is called.");
+			var src = encodedFile.target.result;
+			src1 = src.split("base64,");
+
+			// This is the base64 encoded string of that media file which you will be interested to store in the database(post to the server
+			contentAsBase64EncodedString = src1[1];
+			console.log(contentAsBase64EncodedString);
+						angular.element('#imageDisplay').append("<img src='"+src+"'style='height:250px'></img>");
+			
 		};
 
 		FR.onloadend = function(encodedFile) {
+			/*
 			var src = encodedFile.target.result;
-			src = src.split("base64,");
-
-			// This is the base64 encoded string of that media file which you will be interested to store in the database(post to the server
-			contentAsBase64EncodedString = src[1];
-			console.log(contentAsBase64EncodedString);
+						src = src.split("base64,");
+			
+						// This is the base64 encoded string of that media file which you will be interested to store in the database(post to the server
+						contentAsBase64EncodedString = src[1];
+						console.log(contentAsBase64EncodedString);*/
+			
 		};
 		g=FR.readAsDataURL(file);
-
+}
+else{
+	$('input[type=file]').val('');
+}
 });
 	
 }]); 
